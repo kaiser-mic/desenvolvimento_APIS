d22ae99fef7d45b2472bf993512669f8badc57a6
Pedro Henrique <revannoname@gmail.com>
2025-03-25 11:54:06 -0300

    aula3

---
+++ script.js
@@ -0,0 +1,36 @@
+function lerDados(){
+    var req = new XMLHttpRequest()
+    req.onreadystatechange = function(){
+        if( this.readyState == 4 && this.status == 200){
+            var divDados = document.getElementById("divDados")
+            divDados.innerHTML = this.responseText
+
+        }else if(this.readyState == 4 ){
+            alert(this.statusText)
+        }
+
+
+    }
+
+
+    req.open("GET", "infos.txt", true);
+    req.send()
+}
+function lerNumeros(){
+    var req = new XMLHttpRequest()
+    req.onreadystatechange = function(){
+        if( this.readyState == 4 && this.status == 200){
+            var divNumeros = document.getElementById("divNumeros")
+            divNumeros.innerHTML = this.responseText
+
+        }else if(this.readyState == 4 ){
+            alert(this.statusText)
+        }
+
+
+    }
+    var numero = document.getElementById("txtvalor").value
+
+    req.open("GET", "servidor.php?valor="+numero, true);
+    req.send()
+}
---
+++ servidor.php
@@ -0,0 +1,8 @@
+<?php
+
+$valor = $_GET["valor"];
+$txt = "";
+for ($i = 1; $i <= $valor ; $i++){
+    $txt .= $i."<br>";
+};
+echo $txt;
