f221aa81363e5666ec39cae699512044ba6807c5
Pedro Henrique <revan271205@gmail.com>
2025-04-29 11:48:43 -0300

    commit 29/4

---
+++ aula09-REST/api/index.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const knex = require("knex")
+const errors = require("http-errors")
+
+
+const app = express()
+
+app.use( express.json() )
+app.use( express.urlencoded({extended : true }) )
+
+const PORT = 8001
+const conn = knex( {
+    client : "mysql" ,
+    connection :{
+        host : "localhost",
+        user : "root",
+        password : "",
+        database : "market"
+    }
+} )
+app.get("/", (req, res) =>{
+    res.json( {resposta : "Seja bem-vindo(a) à nossa Loja"} )
+})
+app.get("/product", (req, res, next) =>{
+    conn( "produto" )
+    .then(dados => res.json(dados))
+    .catch(next)
+})
+app.get("/product/:idProd", (req, res, next) =>{
+    const id = req.params.idProd
+    conn( "produto" )
+    .where("id ", id)
+    .first()
+    .then(dados => {
+        if(!dados){
+            return next(errors(404 , "Produto não encontrado"))
+        }
+        res.json(dados)
+
+    })
+    .catch(next)
+})
+
+
+app.listen( PORT , () => {
+    console.log(`servidor executando em: http://localhost:${PORT}`)
+})
