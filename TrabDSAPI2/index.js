2814cae1ab3e138ce3f6dac66a829904423b5a0d
Pedro Henrique <revan271205@gmail.com>
2025-06-06 18:46:30 -0300

    commit signup

--- TrabDSAPI2/index.js
+++ TrabDSAPI2/index.js
@@ -38,3 +38,21 @@ app.get("/products" , (req, res) => {
         res.status(500).json( { error: "Erro ao buscar produtos" } )
     }
 })
+
+app.put("/signup" , (req, res) => {
+    const { nome, altura, nascim, cidade_id } = req.body
+
+    if ( !nome || !altura || !nascim || !cidade_id ) {
+        return res.status(400).json( { error: "Todos os campos são obrigatórios" } )
+    }
+
+    conn( "clientes" )
+        .insert( { nome, altura, nascim, cidade_id } )
+        .then( () => {
+            res.status(201).json( { message: "Usuário cadastrado com sucesso" } )
+        } )
+        .catch( error => {
+            console.error( error )
+            res.status(500).json( { error: "Erro ao cadastrar usuário" } )
+        } )
+})
